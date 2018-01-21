b7ea82a8eacdf6d6e8ec8d051edd02129dcb8f35
Himanshu Palve <himanshupalve1999@gmail.com>
2018-01-22 00:26:19 +0530

    First commit

---
+++ app/src/main/java/com/example/himanshupalve/dicegame/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.himanshupalve.dicegame;
+
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    int Turnscore=0;
+    int compscore=0,playerscore=0;
+    boolean turn,game;
+
+    public MainActivity() {
+        turn = false;
+        game=true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void computer()
+    {
+        if(!game) return;
+        int r=0,d;
+//        ImageView i=(ImageView)findViewById(R.id.DiceImage);
+        Random rand = new Random();
+        while (r<=2)
+        {
+            d=rand.nextInt(6);
+//            switch (d)
+//            {
+//                case 1:
+//                    i.setImageResource(R.drawable.dice1);
+//                    Turnscore=0;
+//                    setscore();
+//                    break;
+//                case 2:i.setImageResource(R.drawable.dice2);
+//                    break;
+//                case 3:i.setImageResource(R.drawable.dice3);
+//                    break;
+//                case 4:i.setImageResource(R.drawable.dice4);
+//                    break;
+//                case 5:i.setImageResource(R.drawable.dice5);
+//                    break;
+//                case 6:i.setImageResource(R.drawable.dice6);
+//                    break;
+//            }
+            if(d!=1)
+            {
+                Turnscore+=d;
+            }
+            else break;
+            r=rand.nextInt(4);
+            disp();
+        }
+        setscore();
+    }
+
+    public void rollDice(View view) {
+        if(!game) return;
+        ImageView i= findViewById(R.id.DiceImage);
+        Random rand=new Random();
+        int d=rand.nextInt(6);
+        switch (d)
+        {
+            case 1:
+                i.setImageResource(R.drawable.dice1);
+                Turnscore=0;
+                setscore();
+                return;
+//                    break;
+            case 2:i.setImageResource(R.drawable.dice2);
+                break;
+            case 3:i.setImageResource(R.drawable.dice3);
+                break;
+            case 4:i.setImageResource(R.drawable.dice4);
+                break;
+            case 5:i.setImageResource(R.drawable.dice5);
+                break;
+            case 6:i.setImageResource(R.drawable.dice6);
+                break;
+        }
+        Turnscore+=d;
+        disp();
+    }
+
+    public void setscore()
+    {
+
+        if(turn)
+            compscore+=Turnscore;
+        else
+            playerscore+=Turnscore;
+//        Toast toast;
+        if(playerscore>=100)
+        {
+            makeText(MainActivity.this, "You Win!!", LENGTH_LONG).show();
+            game=false;
+        }
+        else if(compscore>=100)
+        {
+            makeText(MainActivity.this, "Computer Won;)", LENGTH_LONG).show();
+            game=false;
+        }
+        turn^=true;
+        Turnscore=0;
+        if(turn)
+            computer();
+        disp();
+
+    }
+    public void HoldDice(View view) {
+        if(game) {
+            setscore();
+        }
+    }
+
+    public void disp()
+    {
+        TextView t=(TextView)findViewById(R.id.turnscore);
+        t.setText(Integer.toString(Turnscore));
+        t=(TextView)findViewById(R.id.turn);
+        String s1,s2;
+        String st=" ";
+        TextView s=(TextView)findViewById(R.id.textview);
+        s.setText(Integer.toString(playerscore)+st+Integer.toString(compscore));
+        s1="Computer";
+        s2="YOU";
+        if(turn) {
+            t.setText(s1);
+        }
+        else
+            t.setText(s2);
+    }
+
+    public void Reset(View view) {
+        Turnscore=0;
+        compscore=0;
+        playerscore=0;
+        turn=false;
+        game=true;
+        disp();
+    }
+}
